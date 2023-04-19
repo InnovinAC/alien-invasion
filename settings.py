d5f2d4fbc8865e2c535c01d41d276c90f2e4e7dc
Innovin Anuonye <innovinanuonye@gmail.com>
2023-04-19 22:08:14 +0100

    Initial commit

---
+++ settings.py
@@ -0,0 +1,16 @@
+class Settings:
+    def __init__(self):
+        # screen size
+        self.width = 1000
+        self.height = 600
+
+        # set background color
+        self.bg_color = (10, 100, 230)
+        self.ship_speed = 0.5
+
+        # Bullet settings
+        self.bullet_speed = 0.5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
